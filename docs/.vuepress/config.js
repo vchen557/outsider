8b7d43a2af084a52c69e9f42cd3926ecdab92e95
wangweichen <wangweichen@ahdinfo.com>
2022-08-05 11:41:42 +0800

    fix: 修改配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,9 +4,9 @@ const themeConfig = require('./config/themeConfig.js');
 
 module.exports = {
   theme: 'vdoing', // 使用npm包主题
-  title: "vdoing-template",
+  title: "vchen",
   description: 'vdoing博客主题模板',
-  base: '/vdoing-template/', // 格式：'/<仓库名>/'， 默认'/'
+  base: '/vchen/', // 格式：'/<仓库名>/'， 默认'/'
   markdown: {
     lineNumbers: true, // 代码行号
   },
@@ -25,10 +25,10 @@ module.exports = {
         needComments: true,
         // 其他的 Vssue 配置
         autoCreateIssue: true,
-        clientId: "adb9fb0ac1159e00ce7f",
-        clientSecret: "27da8dc85f808c2bd1b6e44da5ae69c4ddf17d8d",
+        clientId: "052dc99a9ab28de15b3a",
+        clientSecret: "9833fdbe52b75b84a0237ef7f1e723e45cb747d2",
         owner: "eryajf",
-        repo: "vdoing-template",
+        repo: "vchen",
       },
     ],
     // 增强 markdown
